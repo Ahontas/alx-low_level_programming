4d1a4afa1506cb4504d4e350749f88fa1a2d14ad
Ahontas <sharondlamini07@icloud.com>
2022-10-13 20:16:13 -0700

    Puts printing!

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 /**
-* main - print something out
-* 
-* Return:0(sucessful)
-*/
+ * main - print something out
+ *
+ * Return:0(sucessful)
+ */
 int main(void)
 {
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-			return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+	return (0);
 }
