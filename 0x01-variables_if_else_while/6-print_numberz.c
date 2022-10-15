db42232ff684e65abd87b630a275200d06f5d450
Ahontas <sharondlamini07@icloud.com>
2022-10-14 20:49:14 -0700

    digitz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main- printing alphabet
+ *
+ * Return: 0 (succesful)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
