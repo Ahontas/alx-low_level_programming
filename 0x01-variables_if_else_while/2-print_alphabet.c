d417c873d40ddd40a6f4265084eb3d19edcf37bd
Ahontas <sharondlamini07@icloud.com>
2022-10-14 17:57:34 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main- printing alphabet
+ *
+ * Return: 0 (succesful)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+}
