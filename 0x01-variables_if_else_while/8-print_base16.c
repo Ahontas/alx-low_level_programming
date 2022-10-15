f6337a97d8b96aae4df5fd7ca4169a0cd39b6329
Ahontas <sharondlamini07@icloud.com>
2022-10-14 21:00:19 -0700

    a to f

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+ * main- printing alphabet
+ *
+ * Return: 0 (succesful)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+	}
+	for (n = 'a'; n<= 'f'; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
