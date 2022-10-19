9bd68bf7a77c5cf8bf158442ad28247f628d665c
Ahontas <sharondlamini07@icloud.com>
2022-10-19 12:35:24 -0700

    absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - print absolute
+ * parameter to modify:abs
+ * Return: abs value of parameter
+ */
+int _abs(int abs)
+{
+	if (abs < 0)
+	{
+		return (abs * -1);
+	}
+	return (abs);
+}
