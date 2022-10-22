53d07b9548516e95581e79dad06d47caf5dabb1b
Ahontas <sharondlamini07@icloud.com>
2022-10-21 17:23:44 -0700

    Nested loopp 2.0

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * print_diagonal - draws diagonal line
+ * @n: length of diagonal line
+ * return:0
+ */
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n <= 0)
+	{
+		putchar('\n');
+	} else
+	{
+		for (i = 0; i < n; i++)
+		{
+			j = i;
+			while (j >= 0)
+			{
+				if (j == 0)
+				{
+					_putchar('\\');
+				} else
+				{
+					_putchar(' ');
+				}
+				j--;
+			}
+			_putchar('\n');
+		}
+	}
+}
