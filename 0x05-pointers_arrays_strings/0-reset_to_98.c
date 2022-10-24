83bb8046d09829dd39c1f2053cc7b94f1d9f65e9
Ahontas <sharondlamini07@icloud.com>
2022-10-24 16:07:47 -0700

    strings

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * rest_to_98= reset value to 98
+ * rest_to_98 - reset value to 98
  * @n: value to be changed
  * Return 0
  */
 
-void reset_to_98(int *n);
+void reset_to_98(int *n)
 {
 	*n = 98;
 }
