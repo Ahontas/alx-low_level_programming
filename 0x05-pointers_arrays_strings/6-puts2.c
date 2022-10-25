bd61caaee06e6ed3c80694a0ce0b2871414fa1d1
Ahontas <sharondlamini07@icloud.com>
2022-10-24 20:32:07 -0700

    strings and pointers

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+nclude "main.h"
+
+/**
+ * puts2 -  prints every other character of a string
+ * @str:String to print
+ * Return:0
+ */
+
+
+void puts2(char *str)
+{
+	int i;
+
+	i = 0;
+
+	while (str[i])
+	{
+		f (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+		i++;
+	}
+	_putchar('\n');
+}
