9aeab859882f7f79d1761d09d3e2017dfdc0721c
Ahontas <sharondlamini07@icloud.com>
2022-10-31 21:27:43 -0700

    nmore llp

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -21,14 +21,14 @@ unsigned int _strspn(char *s, char *accept)
 	while (str1[i] != '\0')
 	{
 		j = 0;
-		while (str2[j] != '\0') 
+		while (str2[j] != '\0')
 		{
 			if (str2[j] == str1[i])
 			{
 				count++;
 				break;
 			}
-			j++; 
+			j++;
 		}
 		if (s[i] != accept[j])
 		{
