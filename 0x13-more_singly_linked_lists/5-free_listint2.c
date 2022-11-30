0b578f99bdb955889b47f076a61d986141ea63c5
Ahontas <sharondlamini07@icloud.com>
2022-11-30 14:57:32 -0800

    linked lists

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * free_listint2 - Frees a listint_t list.
+ * free_listint2- Frees a listint_t list.
  * @head: A pointer to the address of the head.
  * Description: Sets the head to NULL.
  * Return: void
@@ -16,9 +16,9 @@ void free_listint2(listint_t **head)
 
 	while ((*head) != NULL)
 	{
-		new = (*head)
+		new = *head;
 		(*head) = (*head)->next;
-		free(*head);
+		free(new);
 	}
 	
 	(*head) = NULL;
